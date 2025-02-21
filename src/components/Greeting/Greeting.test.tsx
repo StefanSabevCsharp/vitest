e9e682ec68c1dbde46bc666771ae4ad5143fb960
Stefan Sabev <stevenbg91@gmail.com>
2025-02-21 22:22:07 +0100

    Added simple component and test it

---
+++ src/components/Greeting/Greeting.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {render, screen} from "@testing-library/react"
+import Greeting from "./Greeting";
+import { describe, expect, it } from "vitest";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greeting", () => {
+   it("renders a greeting", () => {
+    render(<Greeting />)
+    expect(screen.getByText("Hello, World!")).toBeInTheDocument();
+   });
+})
---
+++ src/components/Greeting/Greeting.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Greeting({name = "World"} : {name?: string}) {
+    return (
+        <h1>Hello, {name}!</h1>
+    )
+
+}

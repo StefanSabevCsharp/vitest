dfd9fa8903cffe09e6e157fc06578be444ba7ed1
Stefan Sabev <stevenbg91@gmail.com>
2025-02-22 07:54:57 +0100

    Added new counter test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Counter from './components/Counter/Counter'
 import Greeting from './components/Greeting/Greeting'
+import UserProfile from './components/UserProfile/UserProfile'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <Greeting name= {"ivan"}/>
     <Counter />
+    <UserProfile username={"ivan"} email={"ivan@gmail.com"}/>
     </>
   )
 }
--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -1,6 +1,6 @@
 import {cleanup, render, screen } from "@testing-library/react";
 import "@testing-library/user-event"
-import {describe, it, expect, beforeAll, afterAll, afterEach } from "vitest"
+import {describe, it, expect, afterEach } from "vitest"
 import "@testing-library/jest-dom/vitest";
 import Counter from "./Counter";
 import userEvent from "@testing-library/user-event";
@@ -22,4 +22,15 @@ describe("Counter tests", () => {
         const counterValue = screen.getByTestId("counter");
         expect(counterValue).toHaveTextContent("1");
     });
+
+    it("decrements the counter", async () => {
+        render(<Counter />)
+        const button = screen.getByRole("button", { name: "Increment"})
+        await userEvent.click(button);
+        const counterValue = screen.getByTestId("counter");
+        expect(counterValue).toHaveTextContent("1");
+        const decrementButton = screen.getByRole("button", {name : "Decrement"});
+        await userEvent.click(decrementButton);
+        expect(counterValue).toHaveTextContent("0");
+    })
 })
